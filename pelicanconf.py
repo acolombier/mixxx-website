0d1379e9dd3bd727ebda1e37253260314baaf195
danferns <dannywritescode@gmail.com>
2024-07-05 11:29:50 +0530

    add author info (danferns)

--- pelicanconf.py
+++ pelicanconf.py
@@ -269,6 +269,12 @@ AUTHOR_METADATA = {
         "email": "Evelynne@protonmail.com",
         "tagline": "Enthusiastic Mixxx-er",
     },
+    "Daniel Fernandes": {
+        "github": "danferns",
+        "discourse": "danferns",
+        "email": "dannywritescode@gmail.com",
+        "tagline": "GSoC 2024 Student",
+    },
 }
 
 # Needed for Jinja2 markdown filter
